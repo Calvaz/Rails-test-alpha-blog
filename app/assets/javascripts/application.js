d08f5740ec3f7f54e1981c8d6a660e9f52f2a829
Rails Udemy Course <xyz@gmail.com>
2018-12-06 14:45:46 +0000

    Create homepage and add links and style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require rails-ujs
 //= require jquery
 //= require bootstrap-sprockets
-//= require rails-ujs line
 //= require activestorage
 //= require turbolinks
 //= require_tree .
