468d6ce3a81a405b0d1a81f348846941ae427522
Rails Udemy Course <xyz@gmail.com>
2018-12-14 13:51:12 +0000

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:'pages#home'
+  root to: 'pages#home'
   get 'about', to: 'pages#about'
 
   resources :articles
   
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
-end
+  
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+  
+  end
