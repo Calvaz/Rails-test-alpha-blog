75a1ba0b73c791d7c73e24818d851a29b2b68896
Rails Udemy Course <xyz@gmail.com>
2018-12-21 18:20:23 +0000

    Add edit categories feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,10 +19,25 @@ before_action :require_admin, except: [:index, :show]
         end
     end
     
+    def edit
+        @category = Category.find(params[:id])
+    end
+    
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+           flash[:success] = "Category name was successfully updated" 
+           redirect_to categories_path(@category)
+        else
+            render 'edit'
+        end
+    end
+    
     def show
         @category = Category.find(params[:id])
         @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
+    
     
     private
         def category_params
